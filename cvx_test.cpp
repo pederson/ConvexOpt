d36979884a891e30f33916a0e9057329395487fe
pederson <dpederson@utexas.edu>
2016-12-26 22:34:40 -0600

    added Frank-Wolfe, FISTA. Update linear algebra submodule

--- cvx_test.cpp
+++ cvx_test.cpp
@@ -8,43 +8,60 @@ using namespace std;
 
 int main(int argc, char * argv[]){
 
+	// define the data sets
 	unsigned int niters = 20;
 	unsigned int vecsize = 10;
-	Matrix Q = randmat(vecsize, vecsize);
-	Matrix A = (~Q)*Q;
-	Vector b = randvec(vecsize);
-	oracle_fn oracle = [Q,b](const Vector & x)->Vector{return (~Q)*(Q*x-b);};
-	cvx_fn func = [Q,b](const Vector & x)->double{return 0.5*norm_2(Q*x-b);};
-	
-	double mu=1e-2;
-	cvx_fn func_lasso = [Q,b,mu](const Vector & x)->double{return 0.5*norm_2(Q*x-b) + mu*norm_1(x);};
-	oracle_fn subgrad = [Q,b,mu](const Vector & x)->Vector{return (~Q)*(Q*x-b)+mu*sign(x);};
-	proximal_fn prox_lasso = [](const Vector & x)->Vector{
-														  Vector out(x);
-														  for (auto i=0; i<x.length(); i++){
-														  	if (x(i) >= 1) out(i) = x(i)-1;
-														  	else if (x(i) <= 1) out(i) = x(i)+1;
-														  	else out(i) = 0;
-														  }
-														  return out;
-	};
+	Matrix Q = randmatn(10*vecsize, vecsize);
+	Vector b = randvecn(10*vecsize);
+	SparseMatrix S = sprandmatn(100*vecsize, vecsize, 0.3);
+	cout << S.nnz() << "/" << S.rows()*S.cols() << endl;
 
+
+	// ********** DEFINE THE MINIMIZATION PROBLEMS **************
+	// Function 1 ---> min 1/2 ||Q*x-b||^2
+	cvx_fn 				func1 = [Q,b](const Vector & x)->double{return 0.5*norm_2(Q*x-b);};
+	oracle_fn 			grad1 = [Q,b](const Vector & x)->Vector{return (~Q)*(Q*x-b);};
+	partial_grad_fn 	partial1 = [Q,b](const Vector & x, unsigned int i)->Vector{return ~((Vector::dot(Q.row(i),x)-b(i))*(Q.row(i)));};
+	
+	// Function 2 (LASSO) ---> min 1/2 ||Q*x-b||^2 + mu*||x||_1
+	double mu=1e-6;
+	double gam=2;
+	cvx_fn 			func2 = [Q,b,mu](const Vector & x)->double{return 0.5*norm_2(Q*x-b) + mu*norm_1(x);};
+	oracle_fn 		subgrad2 = [Q,b,mu](const Vector & x)->Vector{return (~Q)*(Q*x-b)+mu*sign(x);};
+	proximal_fn 	prox2 = [mu](const Vector & x)->Vector{return sign(x).elem_mult((abs(x)-mu));};
+	oracle_fn 		fws2 = [grad1,gam](const Vector & x)->Vector{Vector gd = grad1(x);
+																 Vector out(x.length()); out.fill(0);
+																 out(argmin(gd)) = -gam*sgn(gd(argmin(gd)));
+																 return out;};
+
+
+
+	// Function 3 (Sparse Regression) ---> min 1/2 ||Sx-b||^2
+	// cvx_fn 		func3 = [S,b](const SparseVector & x)->double{return 0.5*norm_2(S*x-b);};
+
+	// ***********************************************************
+
+
+	// ************** DEFINE STEP SIZE FUNCTIONS *****************
 	// constant step function 
 	stepsize_fn const_step = [](unsigned int k, const Vector & x, const Vector & d, const Vector & gf)->double{return constant_step(0.01);};
-
 	// decreasing step function
-	stepsize_fn decr_step = [](unsigned int k, const Vector & x, const Vector & d, const Vector & gf)->double{return inverse_step(1,2,k);};
-
+	stepsize_fn decr_step = [](unsigned int k, const Vector & x, const Vector & d, const Vector & gf)->double{return inverse_step(1,50,k);};
 	// backtracking line search step
-	stepsize_fn btls = [func](unsigned int k, const Vector & x, const Vector & d, const Vector & gf)->double{return btls_step(0.4, 0.9, x, d, gf, func);};
+	stepsize_fn btls1 = [func1](unsigned int k, const Vector & x, const Vector & d, const Vector & gf)->double{return btls_step(0.4, 0.9, x, d, gf, func1);};
+	stepsize_fn btls2 = [func2](unsigned int k, const Vector & x, const Vector & d, const Vector & gf)->double{return btls_step(0.4, 0.9, x, d, gf, func2);};
+	// ***********************************************************
 
+
+
+
+// **************** PROBLEM 1 ********************
 	// try with a constant step 
 	cout << "************* GD - CONSTANT STEP ************" << endl;
 	Vector x = randvec(vecsize);
 	for (unsigned int k=0; k<niters; k++){
-		x = gradient::gd_step(x, oracle, const_step, k);
-		// cout << "x(" << k+1 << "): " << x ; 
-		cout << " f[x(" << k+1 << ")]: " << func(x) << endl;
+		x = gradient::gd_step(x, grad1, const_step, k);
+		cout << " f[x(" << k+1 << ")]: " << func1(x) << endl;
 	}
 
 
@@ -52,65 +69,87 @@ int main(int argc, char * argv[]){
 	// try with a decreasing step size
 	x = randvec(vecsize);
 	for (unsigned int k=0; k<niters; k++){
-		x = gradient::gd_step(x, oracle, decr_step, k);
-		// cout << "x(" << k+1 << "): " << x ; 
-		cout << " f[x(" << k+1 << ")]: " << func(x) << endl;
+		x = gradient::gd_step(x, grad1, decr_step, k);
+		cout << " f[x(" << k+1 << ")]: " << func1(x) << endl;
 	}
 
 	cout << "************* GD - BTLS STEP ************" << endl;
-	// try with a decreasing step size
+	// try with a btls step size
 	x = randvec(vecsize);
 	for (unsigned int k=0; k<niters; k++){
-		x = gradient::gd_step(x, oracle, btls, k);
-		// cout << "x(" << k+1 << "): " << x ; 
-		cout << " f[x(" << k+1 << ")]: " << func(x) << endl;
+		x = gradient::gd_step(x, grad1, btls1, k);
+		cout << " f[x(" << k+1 << ")]: " << func1(x) << endl;
 	}
 
-
-	cout << "************* SUBGD - BTLS STEP ************" << endl;
-	// try with a decreasing step size
-	x = randvec(vecsize);
-	for (unsigned int k=0; k<niters; k++){
-		x = gradient::subgd_step(x, subgrad, btls, k);
-		// cout << "x(" << k+1 << "): " << x ; 
-		cout << " f[x(" << k+1 << ")]: " << func_lasso(x) << endl;
-	}
-
-
-	cout << "************* PROXGD - BTLS STEP ************" << endl;
-	// try with a decreasing step size
-	x = randvec(vecsize);
-	for (unsigned int k=0; k<niters; k++){
-		x = gradient::proxgd_step(x, subgrad, btls, prox_lasso, k);
-		// cout << "x(" << k+1 << "): " << x ; 
-		cout << " f[x(" << k+1 << ")]: " << func_lasso(x) << endl;
-	}
-
-
-	partial_grad_fn partial_oracle = [Q,b](const Vector & x, unsigned int i)->Vector{return ~(2*(Vector::dot(Q.row(i),x)-b(i))*(Q.row(i)));};
 	cout << "************* SGD - CONSTANT STEP ************" << endl;
-	// try with a decreasing step size
 	x = randvec(vecsize);
 	for (unsigned int k=0; k<niters; k++){
-		x = gradient::sgd_step(x, partial_oracle, const_step, k, A.rows());
-		cout << " f[x(" << k+1 << ")]: " << func(x) << endl;
+		x = gradient::sgd_step(x, partial1, const_step, k, Q.rows());
+		cout << " f[x(" << k+1 << ")]: " << func1(x) << endl;
 	}
 
 
 
 	cout << "************* SVRG - CONSTANT STEP ************" << endl;
-	// try with a decreasing step size
 	x = randvec(vecsize);
+	x = gradient::gd_step(x, grad1, const_step, 0);
 	Vector x_old = x;
-	Vector full_grad = oracle(x_old);
+	Vector full_grad = grad1(x_old)/Q.rows();
 	for (unsigned int m=0; m<5; m++){
-		x_old = x;
-		full_grad = oracle(x_old);
+		
 		for (unsigned int k=0; k<niters; k++){
-			x = gradient::svrg_step(x, x_old, partial_oracle, full_grad, const_step, k, A.rows());
-			cout << " f[x(" << m*niters+k+1 << ")]: " << func(x) << endl;
+			x = gradient::svrg_step(x, x_old, partial1, full_grad, const_step, m*niters+k, Q.rows());
+			cout << " f[x(" << m*niters+k+1 << ")]: " << func1(x) << endl;
 		}
+		x_old = x;
+		full_grad = grad1(x_old)/Q.rows();
 	}
+
+
+
+	
+
+
+
+
+// **************** PROBLEM 2 ********************
+	cout << "************* SUBGD - CONSTANT STEP ************" << endl;
+	x = randvec(vecsize);
+	for (unsigned int k=0; k<niters; k++){
+		x = gradient::subgd_step(x, subgrad2, btls2, k);
+		cout << " f[x(" << k+1 << ")]: " << func2(x) << endl;
+	}
+
+
+	cout << "************* PROXGD - CONSTANT STEP ************" << endl;
+	x = randvec(vecsize);
+	for (unsigned int k=0; k<niters; k++){
+		x = gradient::proxgd_step(x, grad1, const_step, prox2, k);
+		cout << " f[x(" << k+1 << ")]: " << func2(x) << endl;
+	}
+
+
+	cout << "************* FRANK-WOLFE - DECREASING STEP ************" << endl;
+	x = randvec(vecsize);
+	for (unsigned int k=0; k<niters; k++){
+		x = gradient::fw_step(x, fws2, decr_step, k);
+		cout << " f[x(" << k+1 << ")]: " << func2(x) << endl;
+	}
+
+
+	cout << "************* FISTA - DECREASING STEP ************" << endl;
+	x = randvec(vecsize);
+	Vector y(x);
+	double lambda=0;
+	for (unsigned int k=0; k<niters; k++){
+		x = gradient::fista_step(x, y, lambda, grad1, const_step, prox2, k);
+		cout << " f[x(" << k+1 << ")]: " << func2(x) << endl;
+	}
+
+
+
+// *************** PROBLEM 3 ********************
+	
 	
 
 	return 0;
--- include/Gradient.hpp
+++ include/Gradient.hpp
@@ -33,7 +33,8 @@ inline double inverse_step(double a, double b, unsigned int k){return a/(k+b);};
 inline double btls_step(double a, double b, 
 						const Vector & x, const Vector & d,
 						const Vector & grad_f,
-						cvx_fn f){
+						cvx_fn f)
+{
 	double t=1.0;
 	while (f(x+t*d) > (f(x)+a*t*Vector::dot(grad_f,d))){
 		t *= b;
@@ -45,7 +46,8 @@ inline double btls_step(double a, double b,
 inline double sp_btls_step(double a, double b, 
 						const SparseVector & x, const SparseVector & d,
 						const SparseVector & grad_f,
-						sp_cvx_fn f){
+						sp_cvx_fn f)
+{
 	double t=1.0;
 	while (f(x+t*d) > (f(x)+a*t*SparseVector::dot(grad_f,d))){
 		t *= b;
@@ -63,7 +65,8 @@ namespace gradient{
 	Vector gd_step(const Vector & x, 
 				   oracle_fn oracle,
 				   stepsize_fn stepsize,
-				   unsigned int k){
+				   unsigned int k)
+	{
 		// get gradient direction
 		Vector grad = oracle(x);
 		// get stepsize
@@ -81,7 +84,8 @@ namespace gradient{
 	Vector subgd_step(const Vector & x, 
 				   oracle_fn oracle,
 				   stepsize_fn stepsize,
-				   unsigned int k){
+				   unsigned int k)
+	{
 		// get gradient direction
 		Vector subgrad = oracle(x);
 		// get stepsize
@@ -102,7 +106,8 @@ namespace gradient{
 				   oracle_fn oracle,
 				   stepsize_fn stepsize,
 				   proximal_fn prox,
-				   unsigned int k){
+				   unsigned int k)
+	{
 		// get gradient direction
 		Vector grad = oracle(x);
 		// get stepsize
@@ -122,7 +127,8 @@ namespace gradient{
 				   partial_grad_fn partial_grad,
 				   stepsize_fn stepsize,
 				   unsigned int k,
-				   unsigned int n){
+				   unsigned int n)
+	{
 
 		// get a random row
 		unsigned int i=rand()%(n-1);
@@ -148,7 +154,8 @@ namespace gradient{
 				   sp_partial_grad_fn partial_grad,
 				   sp_stepsize_fn stepsize,
 				   unsigned int k,
-				   unsigned int n){
+				   unsigned int n)
+	{
 
 		// get a random row
 		unsigned int i=rand()%(n-1);
@@ -176,16 +183,17 @@ namespace gradient{
 					 const Vector & old_grad,
 					 stepsize_fn stepsize,
 					 unsigned int k,
-					 unsigned int n){
+					 unsigned int n)
+	{
 
 		// get a random row
-		srand(time(NULL));
 		unsigned int i=rand()%(n-1);
 
 		// get gradient direction
 		Vector pgrad = partial_grad(x, i);
 		Vector pgrad_old = partial_grad(x_old, i);
 		Vector gd = pgrad - pgrad_old + old_grad;
+		// Vector gd = pgrad;
 
 
 		// get stepsize
@@ -208,17 +216,17 @@ namespace gradient{
 					 const Vector & old_grad,
 					 sp_stepsize_fn stepsize,
 					 unsigned int k,
-					 unsigned int n){
+					 unsigned int n)
+	{
 
 		// get a random row
-		srand(time(NULL));
 		unsigned int i=rand()%(n-1);
 
 		// get gradient direction
 		SparseVector pgrad = partial_grad(x, i);
 		SparseVector pgrad_old = partial_grad(x_old, i);
 		SparseVector sp_old_grad = old_grad.get_support(pgrad);
-		SparseVector gd = pgrad - pgrad_old + sp_old_grad;
+		SparseVector gd = (pgrad - pgrad_old + sp_old_grad);
 
 
 		// get stepsize
@@ -230,18 +238,48 @@ namespace gradient{
 	}
 
 
-	/*
+
+
 	// frank-wolfe step
-	SparseVector fw_step(const SparseVector & x, 
-					 const SparseVector & x_old,
-					 sp_partial_grad_fn partial_grad,
-					 const Vector & old_grad,
-					 sp_stepsize_fn stepsize,
-					 unsigned int k,
-					 unsigned int n){
+	Vector fw_step(const Vector & x, 
+					 oracle_fn smin,
+					 stepsize_fn stepsize,
+					 unsigned int k)
+	{
+
+		// get the direction s
+		Vector s = smin(x);
+		Vector d = x-s;
+
+		// get stepsize
+		double gamma = stepsize(k, x, -1*d, d);
+
+		// take step
+		Vector xnew = x - gamma*d;
+
+		return xnew;
 	}
 
 
+	/*
+	// sparse frank-wolfe step
+	SparseVector fw_step(const SparseVector & x, 
+					 oracle_fn grad,
+					 sp_frankwolfe s,
+					 sp_stepsize_fn stepsize,
+					 unsigned int k)
+	{
+		// get the gradient
+
+		// get the direction s
+
+		// get stepsize
+
+		// take step
+		SparseVector xnew = (1-gamma)*x + gamma*s;
+	}
+
+	/*
 	// ISTA step
 	Vector ista_step(const Vector & x, 
 					 const Vector & x_old,
@@ -252,18 +290,40 @@ namespace gradient{
 					 unsigned int n){
 
 	}
+	*/
 
 	// Fast-ISTA (FISTA) step
-	Vector fista_step(const Vector & x, 
-					 const Vector & x_old,
-					 partial_grad_fn partial_grad,
-					 const Vector & old_grad,
-					 stepsize_fn stepsize,
-					 unsigned int k,
-					 unsigned int n){
+	Vector fista_step(const Vector & x,
+					  Vector & y,
+					  double & lam,
+					  oracle_fn oracle,
+					  stepsize_fn stepsize,
+					  proximal_fn prox,
+					  unsigned int k){
+		
+		// get gradient direction
+		Vector grad = oracle(x);
+		
+		// get stepsize
+		double gamma = stepsize(k, x, -1*grad, grad);
 
+		// update lambda
+		double lamnew = (1.0+sqrt(1.0+4.0*lam*lam))*0.5;
+		double kappa = (1-lam)/lamnew;
+
+
+		// take step
+		Vector xs = x - gamma*grad;
+		Vector ynew = prox(xs);
+		Vector xnew = (1-kappa)*ynew + kappa*y;
+
+		// update outputs
+		swap(ynew, y);
+		lam = lamnew;
+
+		return xnew;
 	}
-	*/
+	
 
 }
 
